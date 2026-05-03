32ca6d8e30a0d5719caa1ef1e43b1d938ee96722
Nel Dev <nelgonzalezferreras@gmail.com>
2026-05-03 12:14:53 +0200

    test: improve login coverage and smoke speed

--- cypress/e2e/step_definitions/login.steps.ts
+++ cypress/e2e/step_definitions/login.steps.ts
@@ -1,4 +1,9 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import {
+  DataTable,
+  Given,
+  When,
+  Then,
+} from "@badeball/cypress-cucumber-preprocessor";
 import LoginPage from "../../pages/loginPage";
 import InventoryPage from "../../pages/inventoryPage";
 
@@ -10,10 +15,28 @@ Given("I open the Sauce Demo login page", () => {
   loginPage.assertOnLoginPage();
 });
 
-When("I log in with standard user credentials", () => {
-  loginPage.login("standard_user", "secret_sauce");
-});
+When(
+  "I log in with username {string} and password {string}",
+  (username: string, password: string) => {
+    loginPage.login(username, password);
+  },
+);
 
 Then("I should see the inventory page", () => {
   inventoryPage.assertInventoryPage();
+});
+
+Then("I should see the login error {string}", (errorMessage: string) => {
+  loginPage.assertErrorMessage(errorMessage);
+});
+
+Then("I should remain on the login page", () => {
+  loginPage.assertOnLoginPage();
+});
+
+When("I validate these invalid login attempts", (loginAttempts: DataTable) => {
+  loginAttempts.hashes().forEach(({ username, password, errorMessage }) => {
+    loginPage.login(username, password);
+    loginPage.assertErrorMessage(errorMessage);
+  });
 });
